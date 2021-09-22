cbd4604a8059521f86bd9c04e43c4321f845719d
diego@dieguntuk <falassionl@gmail.com>
2021-09-22 11:07:30 -0300

    fix in som still tracked files

--- sales-system/api/Controllers/ClienteController.cs
+++ sales-system/api/Controllers/ClienteController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using sales_system.Models;
 using sales_system.Models.Responses;
+using sales_system.Models.Requests;
 
 namespace sales_system.Controllers
 {
@@ -17,13 +18,39 @@ namespace sales_system.Controllers
         public IActionResult Get() 
         {
             Response response = new Response();
-            response.status = 0;
 
             try
             {
                 using(VentaRealContext db = new VentaRealContext())
                 {
                     response.data = db.Clientes.ToList();
+                    response.status = 1;
+                }
+            }
+            catch(Exception ex)
+            {
+                response.status = 0;
+                response.message = ex.Message;
+            }
+
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public IActionResult Add(ClienteRequest clienteRequest) 
+        {
+            Response response = new Response();
+
+            try
+            {
+                using(VentaRealContext db = new VentaRealContext())
+                {
+                    Cliente cliente = new Cliente();
+                    cliente.Nombre = clienteRequest.Nombre;
+
+                    db.Clientes.Add(cliente);
+                    db.SaveChanges();
+                    
                     response.status = 1;
                 }
             }
