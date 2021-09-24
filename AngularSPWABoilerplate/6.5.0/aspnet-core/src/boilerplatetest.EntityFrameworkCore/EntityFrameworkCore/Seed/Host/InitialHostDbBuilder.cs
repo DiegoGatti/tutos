b78436dbadcb0ea18c09cbdf310cae08f3a58d28
diego@dieguntuk <falassionl@gmail.com>
2021-09-24 17:25:15 -0300

    just added a new version of the project from zero, for some reason angular wouldnt work

---
+++ AngularSPWABoilerplate/6.5.0/aspnet-core/src/boilerplatetest.EntityFrameworkCore/EntityFrameworkCore/Seed/Host/InitialHostDbBuilder.cs
@@ -0,0 +1,22 @@
+﻿namespace boilerplatetest.EntityFrameworkCore.Seed.Host
+{
+    public class InitialHostDbBuilder
+    {
+        private readonly boilerplatetestDbContext _context;
+
+        public InitialHostDbBuilder(boilerplatetestDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Create()
+        {
+            new DefaultEditionCreator(_context).Create();
+            new DefaultLanguagesCreator(_context).Create();
+            new HostRoleAndUserCreator(_context).Create();
+            new DefaultSettingsCreator(_context).Create();
+
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ AngularSPWABoilerplate/6.5.0/aspnet-core/src/boilerplatetest.EntityFrameworkCore/EntityFrameworkCore/boilerplatetestDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Abp.Zero.EntityFrameworkCore;
+using boilerplatetest.Authorization.Roles;
+using boilerplatetest.Authorization.Users;
+using boilerplatetest.MultiTenancy;
+
+namespace boilerplatetest.EntityFrameworkCore
+{
+    public class boilerplatetestDbContext : AbpZeroDbContext<Tenant, Role, User, boilerplatetestDbContext>
+    {
+        /* Define a DbSet for each entity of the application */
+        
+        public boilerplatetestDbContext(DbContextOptions<boilerplatetestDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
