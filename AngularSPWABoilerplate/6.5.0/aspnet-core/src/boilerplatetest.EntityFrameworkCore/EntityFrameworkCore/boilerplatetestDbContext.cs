99711265a9d982e730a93148e4214f5aa92bfe76
diego@dieguntuk <falassionl@gmail.com>
2021-09-27 17:16:35 -0300

    added entities

--- AngularSPWABoilerplate/6.5.0/aspnet-core/src/boilerplatetest.EntityFrameworkCore/EntityFrameworkCore/boilerplatetestDbContext.cs
+++ AngularSPWABoilerplate/6.5.0/aspnet-core/src/boilerplatetest.EntityFrameworkCore/EntityFrameworkCore/boilerplatetestDbContext.cs
@@ -3,12 +3,14 @@ using Abp.Zero.EntityFrameworkCore;
 using boilerplatetest.Authorization.Roles;
 using boilerplatetest.Authorization.Users;
 using boilerplatetest.MultiTenancy;
+using boilerplatetest.Core.Entities;
 
 namespace boilerplatetest.EntityFrameworkCore
 {
     public class boilerplatetestDbContext : AbpZeroDbContext<Tenant, Role, User, boilerplatetestDbContext>
     {
         /* Define a DbSet for each entity of the application */
+        public DbSet<Client> Clients { get; set; }
         
         public boilerplatetestDbContext(DbContextOptions<boilerplatetestDbContext> options)
             : base(options)
